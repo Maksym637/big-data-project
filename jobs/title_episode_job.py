008c19436a7c06117e8a116b3175f07d0d30358a
maksym <maksym.oliinyk.knm.2020@lpnu.ua>
2023-11-14 00:04:32 +0200

    Add helper method write_results_to_file + perform code refactoring

--- jobs/title_episode_job.py
+++ jobs/title_episode_job.py
@@ -10,27 +10,32 @@ from jobs.base_job import TSVData
 class TitleEpisodeData(TSVData):
     """Business questions for the `title.episode` data"""
 
-    def get_business_question_1(self) -> DataFrame:
+    def get_title_episodes_with_provided_season_number(self, threshold=6) -> DataFrame:
         """
         Get all title episodes where season number is equal 6
         """
         return self.tsv_df.filter(
-            condition=(self.tsv_df[TitleEpisodeModel.season_number] == 6)
+            condition=(self.tsv_df[TitleEpisodeModel.season_number] == threshold)
         )
 
-    def get_business_question_2(self) -> DataFrame:
+    def get_first_title_episodes_in_interval(
+        self,
+        season_number_threshold=1,
+        episode_number_threshold=10,
+        limit_num=5
+    ) -> DataFrame:
         """
         Get first 5 title episodes where season number is equal 1
         and episode number is less than 10
         """
         return self.tsv_df.filter(
             condition=(
-                (self.tsv_df[TitleEpisodeModel.season_number] == 1) &
-                (self.tsv_df[TitleEpisodeModel.episode_number] < 10)
+                (self.tsv_df[TitleEpisodeModel.season_number] == season_number_threshold) &
+                (self.tsv_df[TitleEpisodeModel.episode_number] < episode_number_threshold)
             )
-        ).limit(num=5)
+        ).limit(num=limit_num)
 
-    def get_business_question_3(self) -> DataFrame:
+    def get_first_latest_title_episodes(self, limit_num=5) -> DataFrame:
         """
         Get first 5 title episodes with the latest season number
         """
@@ -38,5 +43,5 @@ class TitleEpisodeData(TSVData):
             self.tsv_df
                 .groupBy(self.tsv_df[TitleEpisodeModel.parent_tconst])
                 .max(TitleEpisodeModel.season_number)
-                .limit(num=5)
+                .limit(num=limit_num)
         )
--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 from utils.constants import INPUT_DATA_PATH
-from utils.schemas import title_episode_schema
+from utils.helpers import write_results_to_file
+from utils.schemas import (
+    title_episode_schema,
+    title_ratings_schema
+)
 
 from jobs.title_episode_job import TitleEpisodeData
-from jobs.title_ratings_job import TitleRatingsJob
+from jobs.title_ratings_job import TitleRatingsData
 
 spark_session = (
     SparkSession.builder
@@ -18,40 +22,32 @@ spark_session = (
 )
 
 if __name__ == '__main__':
-    # Import `title.episode` data
+    # Import the following data frames
     title_episode_df = TitleEpisodeData(
         spark_session=spark_session,
         path=f'{INPUT_DATA_PATH}/title.episode.tsv',
         schema=title_episode_schema
     )
 
-    # Import `title.ratings` data
-    title_ratings_df = TitleRatingsJob(
+    title_ratings_df = TitleRatingsData(
         spark_session=spark_session,
         path=f'{INPUT_DATA_PATH}/title.ratings.tsv',
         schema=title_ratings_schema
     )
 
-    # Get general information about the data
+    # Definition of business questions
     title_episode_df.get_first_rows()
-    title_episode_df.get_schema_info()
-    title_episode_df.get_basic_statistics()
-
-    # Get results to 3 business questions
-    title_episode_df.get_business_question_1().show()
-    title_episode_df.get_business_question_2().show()
-    title_episode_df.get_business_question_3().show()
-
-    # Get general information about the data
     title_ratings_df.get_first_rows()
-    title_ratings_df.get_schema_info()
-    title_ratings_df.get_basic_statistics()
 
-    # Get results to 3 business questions
-    title_ratings_df.get_top_rated_titles().show()
-    title_ratings_df.count_highly_rated_titles().show() 
-    title_ratings_df.get_most_voted_titles().show()
-    title_ratings_df.count_titles_with_few_votes().show()
-    title_ratings_df.average_votes_per_rating_interval().show()
+    processed_df_list = [
+        # BQ for the `title.episode` data
+        (title_episode_df.get_title_episodes_with_provided_season_number(), 'title_episode_bq_1'),
+        (title_episode_df.get_first_title_episodes_in_interval(), 'title_episode_bq_2'),
+        (title_episode_df.get_first_latest_title_episodes(), 'title_episode_bq_3'),
+        # BQ for the `title.ratings` data
+        (title_ratings_df.count_highly_rated_titles(), 'title_ratings_bq_1'),
+        (title_ratings_df.count_titles_with_few_votes(), 'title_ratings_bq_2'),
+        (title_ratings_df.get_average_votes_per_rating_interval(), 'title_ratings_bq_3')
+    ]
 
-    # TODO
+    write_results_to_file(processed_df_list)
