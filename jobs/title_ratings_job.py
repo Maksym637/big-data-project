4242442913db0701234035d632a2a1f662099ca5
EyR1oN <91558615+EyR1oN@users.noreply.github.com>
2023-12-03 21:16:26 +0200

    WIP

--- jobs/title_ratings_job.py
+++ jobs/title_ratings_job.py
@@ -41,65 +41,28 @@ class TitleRatingsData(TSVData):
                 .orderBy('rating_interval')
         )
     
-    def get_top_rated_titles_by_genre(self, genre_table, top_n=10) -> DataFrame:
+    def running_total_and_average(self):
         """
-        Find the top N rated titles in each genre.
+        Calculate the running total of votes and the average rating for each title,
+        ordered by average rating in descending order.
         """
-        # Joining with genre table
-        joined_df = self.tsv_df.join(genre_table, self.tsv_df.titleId == genre_table.titleId, 'inner')
-
-        # Window function to rank titles within each genre based on average rating
-        window_spec = Window.partitionBy(genre_table['genre']).orderBy(self.tsv_df[TitleRatingsModel.average_rating].desc())
-
-        # Applying the window function and filtering the top N for each genre
+        window_spec = Window.orderBy(F.desc("averageRating"))
         return (
-            joined_df
-                .withColumn('rank', F.rank().over(window_spec))
-                .filter(F.col('rank') <= top_n)
-                .select(genre_table['genre'], self.tsv_df['titleId'], self.tsv_df[TitleRatingsModel.average_rating])
+            self.tsv_df
+                .withColumn("RunningTotalVotes", F.sum("numVotes").over(window_spec))
+                .withColumn("RunningAvgRating", F.avg("averageRating").over(window_spec))
+                .select("tconst", "averageRating", "numVotes", "RunningTotalVotes", "RunningAvgRating")
         )
     
-    def get_rating_change_over_years(self, titles_table, pivot_year) -> DataFrame:
+    def highest_rating_in_groups_of_five(self):
         """
-        Calculate the difference in average rating for titles before and after a specific year.
+        Identify the highest average rating within each group of 5 titles,
+        sorted by number of votes in descending order.
         """
-        # Joining with titles table to get release years
-        joined_df = self.tsv_df.join(titles_table, self.tsv_df.titleId == titles_table.titleId, 'inner')
-
-        # Window functions for average ratings before and after the pivot year
-        window_spec_before = Window.partitionBy().orderBy().rowsBetween(Window.unboundedPreceding, pivot_year)
-        window_spec_after = Window.partitionBy().orderBy().rowsBetween(pivot_year, Window.unboundedFollowing)
-
-        # Applying window functions to calculate average ratings
+        window_spec = Window.orderBy(F.desc("numVotes")).rowsBetween(0, 4)
         return (
-            joined_df
-                .withColumn('avg_rating_before', F.avg(self.tsv_df[TitleRatingsModel.average_rating]).over(window_spec_before))
-                .withColumn('avg_rating_after', F.avg(self.tsv_df[TitleRatingsModel.average_rating]).over(window_spec_after))
-                .select('titleId', 'avg_rating_before', 'avg_rating_after')
-        )
-    
-    def get_most_consistent_genres_in_ratings(self, genre_table, min_titles=50) -> DataFrame:
-        """
-        Identify genres with the least variance in ratings.
-        """
-        # Joining with genre table
-        joined_df = self.tsv_df.join(genre_table, self.tsv_df.titleId == genre_table.titleId, 'inner')
-
-        # Grouping by genre and calculating variance in ratings
-        genre_variance_df = (
-            joined_df
-                .groupBy(genre_table['genre'])
-                .agg(F.variance(self.tsv_df[TitleRatingsModel.average_rating]).alias('rating_variance'))
-        )
-
-        # Filtering genres with a minimum number of titles
-        count_df = joined_df.groupBy(genre_table['genre']).count()
-        filtered_genres = count_df.filter(F.col('count') >= min_titles).select('genre')
-
-        # Joining to get consistent genres
-        return (
-            genre_variance_df
-                .join(filtered_genres, 'genre', 'inner')
-                .orderBy('rating_variance')
+            self.tsv_df
+                .withColumn("HighestRatingInGroup", F.max("averageRating").over(window_spec))
+                .select("tconst", "averageRating", "numVotes", "HighestRatingInGroup")
         )
 
